0aa3d7a8e4080219b4992e5aa0c7fa64d7c68bee
Anna2000T <153619435+Anna2000T@users.noreply.github.com>
2024-01-24 09:09:08 +0100

    Add files via upload

---
+++ SoutIn.java
@@ -0,0 +1,23 @@
+
+
+public class Soutln{
+    public static void main(String[] args) {
+        
+
+        String msg ="I am happy today";
+        int a = 100;
+        int o = 0226;
+        int h = 0x96;
+        double d = 10;
+
+        System.out.println(msg);
+        System.out.println(a);
+        System.out.println(o);
+        System.out.println(h);
+        System.out.println(d);
+       
+
+    
+    }
+
+}
